ffbbc69a996e0fe61ec2b3276f089cb57c6a0faf
vengadoranal <geralejo95@gmail.com>
2020-10-12 12:24:30 +0200

    Univariable terminado
    Ejercicio 1 con univariable terminado, se añadio el fichero para normalizar

--- HW1/codigo/main.m
+++ HW1/codigo/main.m
@@ -10,11 +10,14 @@ dataset = load('../datahw1.csv');
 X = dataset(:,1:14);
 y = dataset(:,15);
 
+X = featureNormalize(X);
+
 m = length(y);
 
 x_size = size(X,2);
 
-%%Ejercicio 1 %%
+%% EJERCICIO 1 %%
+%% APARTADO A
 %% Calculo de la regresion univariable tomando el conjunto de datos como test
 %% Calculamos regresion para cada columna
 
@@ -22,7 +25,7 @@ for i = 1:x_size
 
   %guardamos en una variable temporal la columna correspondiente a la iteracion  
   X_temp = X(:,i);
-  %a�adimos una columna de unos
+  %anadimos una columna de unos
   X_temp = [ones(m,1), X_temp];
   
   %calculo de theta
@@ -37,5 +40,104 @@ for i = 1:x_size
 
 endfor
 
+%clear X_temp
+clear X_temp;
+
+%APARTADO B
 %ordena la matriz en base a la segunda columna con los errores y guardamos los 5 mejores
 best5 = sortrows(error_list, 2)(1:5,:);
+%Imprime los 5 valores
+best5
+
+%repite con los 5 mejores valores
+for i = 1:size(best5,1)
+  
+  %Limpiamos la variable temporal en cada iteracion
+  clear X_temp;
+  
+  col = best5(i,1);
+  X_temp = X(:,col);
+  
+  %anadimos una columna de unos
+  X_temp = [ones(m,1), X_temp];
+  
+  %calculo de theta
+  theta = normalEqn(X_temp, y);
+  
+  %calulo del error para esa columna
+  pre = X_temp * theta;
+
+  error = absError(m, pre, y);
+  %guardamos el numero de la columna con su error correspondiente
+  error_listBest5(i,:) = error;
+  
+endfor
+
+%APARTADO E
+
+%Separamos conjuntos de train y test por cada columna del ocnjunto de X
+%asignamos el porcentaje de entrenamiento el resto sera test
+percent = 0.7;
+
+for i = 1:x_size
+  
+  %Limpiamos la variable temporal en cada iteracion
+  clear X_temp;
+  %guardamos en una variable temporal la columna correspondiente a la iteracion  
+  X_temp = X(:,i);
+  %realizamos la separacion en train y test
+  [X_train,y_train, X_test, y_test] = holdout(X_temp, y, m, percent);
+  
+  m_train = length(y_train);
+  m_test = length(y_test);
+  
+  %anadimos una columna de unos
+  X_train = [ones(m_train,1), X_train];
+  X_test = [ones(m_test,1), X_test];
+  
+  %calculo de theta
+  theta = normalEqn(X_train, y_train);
+  
+  %calulo del error para esa columna
+  pre = X_test * theta;
+
+  error = absError(m_test, pre, y_test);
+  %guardamos el numero de la columna con su error correspondiente
+  error_listHoldout(i,:) = [i,error];
+
+endfor
+
+
+%imprime los mejores 5 resultados
+best5Holdout = sortrows(error_listHoldout, 2)(1:5,:);
+
+%repite con los 5 mejores valores
+for i = 1:size(best5Holdout,1)
+  
+  %Limpiamos la variable temporal en cada iteracion
+  clear X_temp;
+  
+  col = best5Holdout(i,1);
+  X_temp = X(:,col);
+  
+  %realizamos la separacion en train y test
+  [X_train,y_train, X_test, y_test] = holdout(X_temp, y, m, percent);
+  
+  m_train = length(y_train);
+  m_test = length(y_test);
+  
+  %anadimos una columna de unos
+  X_train = [ones(m_train,1), X_train];
+  X_test = [ones(m_test,1), X_test];
+  
+  %calculo de theta
+  theta = normalEqn(X_train, y_train);
+  
+  %calulo del error para esa columna
+  pre = X_test * theta;
+
+  error = absError(m_test, pre, y_test);
+  %guardamos el numero de la columna con su error correspondiente
+  error_listBest5holdout(i,:) = error;
+  
+endfor
