68a4397f20cc54cfa6d2dbe37fe045d368c6b934
vengadoranal <geralejo95@gmail.com>
2020-10-12 13:46:36 +0200

    Ejercicio1 multivariable
    Sin terminar

--- HW1/codigo/main.m
+++ HW1/codigo/main.m
@@ -17,6 +17,7 @@ m = length(y);
 x_size = size(X,2);
 
 %% EJERCICIO 1 %%
+%% UNIVARIABLE
 %% APARTADO A
 %% Calculo de la regresion univariable tomando el conjunto de datos como test
 %% Calculamos regresion para cada columna
@@ -141,3 +142,80 @@ for i = 1:size(best5Holdout,1)
   error_listBest5holdout(i,:) = error;
   
 endfor
+
+
+%%%------------------------------------------------------------------------------------
+%%EJERCICIO 1
+%%MULTIVARIABLE%%
+%Apartado A
+
+%anadimos una columna de unos
+  X = [ones(m,1), X];
+  
+  %calculo de theta
+  theta = normalEqn(X, y);
+  
+  %calulo del error para esa columna
+  pre = X * theta;
+
+  error = absError(m, pre, y);
+  
+%%APARTADO E
+
+%% volvemos a guardar el conjunto original
+X = dataset(:,1:14);
+
+%realizamos la separacion en train y test
+[X_train,y_train, X_test, y_test] = holdout(X, y, m, percent);
+  
+  m_train = length(y_train);
+  m_test = length(y_test);
+  
+  %anadimos una columna de unos
+  X_train = [ones(m_train,1), X_train];
+  X_test = [ones(m_test,1), X_test];
+  
+  %calculo de theta
+  theta = normalEqn(X_train, y_train);
+  
+  %calulo del error para esa columna
+  pre = X_test * theta;
+
+  errorHoldout = absError(m_test, pre, y_test);
+  
+  
+  
+  
+##%%APARTADO B
+##%imprime los mejores 5 resultados
+##best5 = sortrows(error_list, 2)(1:5,:);
+##%Imprime los 5 valores
+##best5
+##
+##%%APARTADO C
+##%%Repetimos usando los 5 mejores resultados
+##%%Guardamos las mejores 5 columnas
+##col = best5(1,1);
+##X_temp = X(:,col);
+##
+##for i = 2:size(best5,1)
+##    
+##  col = best5(i,1);
+##  X_temp = [X_temp, X(:,col)];
+##
+##endfor
+##
+##
+##%calculo de theta
+##  theta = normalEqn(X_temp, y);
+##  
+##  %calulo del error para esa columna
+##  pre = X_temp * theta;
+##
+##  error = absError(m, pre, y);
+  
+  
+  
+  
+  
+  
